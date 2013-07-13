c3887797ec3218a8b713c371d938a39e9bb1347b
Cyrus Kazemi <cyruskazemi@gmail.com>
2013-07-12 17:42:04 -0700

    Added parameter inputs

--- minesweeper.js
+++ minesweeper.js
@@ -4,13 +4,46 @@
 /* http://eloquentjavascript.net/
 /*------------------------------------------------------------*/
 
-var constants = {
+var parameters = {
 	BOARD_WIDTH : 8,
 	BOARD_HEIGHT : 8,
 	INITIAL_MINES : 10,
-	BOMB_TILE : '*',
-	EMPTY_TILE : ' '
+	MINE_TILE : '*',
+	EMPTY_TILE : ' ',
+	TILE_WIDTH : 28
 };
+
+function update_parameters() {
+	var width = parseInt($('#BOARD_WIDTH').val());
+	var height = parseInt($('#BOARD_HEIGHT').val());
+	var mines = parseInt($('#INITIAL_MINES').val());
+	if (typeof width !== "number" ||
+		typeof height !== "number" ||
+		typeof mines !== "number" ||
+		isNaN(width) ||
+		isNaN(height) ||
+		isNaN(mines) ||
+		width < 1 ||
+		height < 1 ||
+		mines < 1) {
+		alert('Please enter positive numeric values');
+		return false;
+	} else {
+		if(mines > width * height) {
+			alert('Too many mines. Please choose a smaller amount');
+		} else {
+			parameters.BOARD_WIDTH = width;
+			parameters.BOARD_HEIGHT = height;
+			parameters.INITIAL_MINES = mines;
+			$('#tiles').width((width + 1) * parameters.TILE_WIDTH);
+			return true;
+		}	
+	}
+}
+
+/*--------------------*/
+/* Point & Directions
+/*--------------------*/
 
 function Point(x, y) {
 	this.x = x;
@@ -20,6 +53,10 @@ function Point(x, y) {
 Point.prototype.add = function(other) {
 	return new Point(this.x + other.x, this.y + other.y);
 };
+
+Point.prototype.get_index = function() {
+	return this.x + this.y * parameters.BOARD_WIDTH;
+}
 
 var directions = {
 	'n'  : new Point( 0, -1),
@@ -37,65 +74,81 @@ var directions = {
 /*--------------------*/
 
 function MinesweeperGame() {
-	this.board = new MinesweeperBoard(constants.BOARD_WIDTH, constants.BOARD_HEIGHT, constants.INITIAL_MINES);
+	update_parameters();
+	this.game_in_play = true;
+	this.board = new MinesweeperBoard(parameters.BOARD_WIDTH, parameters.BOARD_HEIGHT, parameters.INITIAL_MINES);
 }
 
 MinesweeperGame.prototype.validate = function() {
 	var message = "You lost :( Press New Game to try again.";
-	if(this.board.check_complete())
+	if(!this.game_in_play)
+		message = "Current game is over. Please start a new game.";
+	else if(this.board.check_complete())
 		message = "You won! Congrats dawg.";
+	this.game_in_play = false;
 	alert(message);
 };
 
 MinesweeperGame.prototype.cheat = function() {
-	this.board.show_bombs();
+	this.board.show_mines();
 };
 
-MinesweeperGame.prototype.processMove = function(index) {
+MinesweeperGame.prototype.processMove = function(dom_tile) {
+	var index = parseInt(dom_tile.id);
 	var point = new Point(Math.floor(index % this.board.width), Math.floor(index / this.board.height));
-	var tile = this.board.value_at(point);
+	var tile = this.board.tile_at(point);
 	var content = tile.get_content();
-	if (!tile.has_bomb()) {
-		//check adjacent tiles with bombs
-		var adjacent_tile_indexes = this.get_adjacent_tiles(point);		
-		var bomb_count = this.get_adjacent_bomb_count(adjacent_tile_indexes);
-		
-		if(bomb_count == 0) {
-			content = bomb_count;
-			tile.clicked = true;
-			tile.image = 'empty';
-			for(adjacent_tile in adjacent_tile_indexes) {
-				this.board.tiles[adjacent_tile].clicked = true;
-				this.board.tiles[adjacent_tile].image = 'empty';
-			}				
-		} 	
-	} else {
-		console.log('BOMB! ' + content);
+
+	//check adjacent tiles with mines
+	if(!tile.has_mine()) {		
+		var adjacent_tile_points = this.get_adjacent_tiles(point);		
+		var mine_count = this.get_adjacent_mine_count(adjacent_tile_points);		
+		if(mine_count == 0) {
+			for(adjacent_tile in adjacent_tile_points) {
+				current_tile = this.board.tile_at(adjacent_tile_points[adjacent_tile]);
+				current_tile.clicked = true;
+				$('#' + adjacent_tile_points[adjacent_tile].get_index() + '_tile').addClass('clicked');
+			}						
+		}
+		content = mine_count;	
 	}
-	return content;
+
+	// Update tile state
+	tile.clicked = true;
+	$(dom_tile).addClass('clicked');
+	$(dom_tile).text(content);
+
+
+	// End game if mine was uncovered
+	if(tile.has_mine()) {
+		alert('BOOM! You lost :( Press New Game to try again.');
+		this.stop_game();
+	}	
+};
+
+MinesweeperGame.prototype.stop_game = function() {
+	this.game_in_play = false;
 };
 
 MinesweeperGame.prototype.get_adjacent_tiles = function(point) {
 	var adjacent_tile_points = [];
 	for(var direction in directions) {
 		var current_point = point.add(directions[direction]);
-		console.log(directions[direction]);
 		if(this.board.within_boundaries(current_point)) {
 			adjacent_tile_points.push(current_point);
 		}
 	}
-	console.log('tiles adjacent to ' + point.x + ',' + point.y + ': ' + adjacent_tile_points.toString());
 	return adjacent_tile_points;
 }
 
-MinesweeperGame.prototype.get_adjacent_bomb_count = function(adjacent_tile_points) {
-	var adjacent_bombs = 0;
+MinesweeperGame.prototype.get_adjacent_mine_count = function(adjacent_tile_points) {
+	var adjacent_mines = 0;
 	for(var current_point in adjacent_tile_points) {
-		if(this.board.value_at(adjacent_tile_points[current_point]).get_content() == '*') {
-			adjacent_bombs++;
+		if(this.board.tile_at(adjacent_tile_points[current_point]).get_content() == '*') {
+			adjacent_mines++;
 		}
 	}
-	return adjacent_bombs;
+	return adjacent_mines;
 };
 
 /*--------------------*/
@@ -110,42 +163,39 @@ function MinesweeperBoard(width, height, mines) {
 	for(var y = 0; y < this.height; y++) {
 		for(var x = 0; x < this.width; x++) {
 			var index = x + y * this.width;
-			this.tiles[index] = new MinesweeperTile(constants.EMPTY_TILE);
+			this.tiles[index] = new MinesweeperTile(parameters.EMPTY_TILE);
 			$('#tiles').append('<span class="tile" id="' + index + '_tile">&nbsp;</span>');
-		}		
+		}
 	}
 
-	// Put outside of inital loop to better distribute bombs
+	// Put outside of inital loop to better distribute mines
 	while(mines > 0) {
 		var index = Math.floor(Math.random() * this.tiles.length);
-		if(this.tiles[index].get_content() != constants.BOMB_TILE) {
-			this.tiles[index].set_content(constants.BOMB_TILE);
+		if(this.tiles[index].get_content() != parameters.MINE_TILE) {
+			this.tiles[index].set_content(parameters.MINE_TILE);
 			mines--;
 		}
 	}
 }
 
-MinesweeperBoard.prototype.value_at = function(point) {
+MinesweeperBoard.prototype.tile_at = function(point) {
 	return this.tiles[point.x + this.width * point.y];
 };
 
 MinesweeperBoard.prototype.check_complete = function() {
 	for(i = 0; i < this.tiles.length; i++) {
-		console.log(this.tiles[i].clicked + ' ' + this.tiles[i].get_content());
-		if(this.tiles[i].clicked == false && this.tiles[i].get_content() == 'empty') {
-			console.log('empty found');
+		if(this.tiles[i].clicked == false && this.tiles[i].get_content() == parameters.EMPTY_TILE) {
 			return false;
 		}			
 	}
 	return true;
 };
 
-MinesweeperBoard.prototype.show_bombs = function() {
+MinesweeperBoard.prototype.show_mines = function() {
 	var tiles = this.tiles;
 	for(i = 0; i < tiles.length; i++) {
-		if(tiles[i].clicked == false && tiles[i].get_content() == constants.BOMB_TILE) {
-			console.log('bomb at ' + i);
-			$('#'+i+'_tile').text(tiles[i].get_content())
+		if(tiles[i].clicked == false && tiles[i].get_content() == parameters.MINE_TILE) {
+			$('#'+i+'_tile').text(tiles[i].get_content());
 		}			
 	}
 };
@@ -163,7 +213,7 @@ function MinesweeperTile(content) {
 	this.clicked = false;
 }
 
-MinesweeperTile.prototype.has_bomb = function() {
+MinesweeperTile.prototype.has_mine = function() {
 	return this.content == '*';
 };
 
@@ -184,14 +234,16 @@ $(document).ready(function() {
 	window.minesweeper = new MinesweeperGame();
 
 	// Set up interactions
-	$('.tile').on('click', function(){
-		$(this).addClass('clicked');
-		$(this).text(minesweeper.processMove(parseInt(this.id)));
+	$('#tiles').on('click', '.tile', function(){
+		if(window.minesweeper.game_in_play)
+			window.minesweeper.processMove(this);		
 	});
 
 	$('#new_game').on('click', function(){
-		$('#tiles').empty();
-		window.minesweeper = new MinesweeperGame();
+		if(update_parameters()) {
+			$('#tiles').empty();
+			window.minesweeper = new MinesweeperGame();
+		}
 	});
 
 	$('#smiley').on('click', function(){
